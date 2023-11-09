01cf4170b00a4b693bf3726f8b084bbe1a1dcfd6
garrett534 <garrett.534@buckeyemail.osu.edu>
2023-11-09 11:12:19 -0500

    Fixing MapAndCalib

--- Arduino/MapAndCalibFunc1.1/MapAndCalibFunc1.1.ino
+++ Arduino/MapAndCalibFunc1.1/MapAndCalibFunc1.1.ino
@@ -13,10 +13,9 @@ const int sampleSize = 20;
 // Take samples and return the average
 int ReadAxis(int axisPin)
 {
-  long reading = 0;
+  int reading = 0;
   analogRead(axisPin);
-  delay(1);
-  for (int i = 0; i < sampleSize; i++)
+  for (int k = 0; k < sampleSize; k++)
   {
   reading += analogRead(axisPin);
   }
@@ -34,7 +33,7 @@ typedef struct struct_message {
 
 // Global Calibration variables
 int offset=0;
-int numSamples = 200;
+int numSamples = 2000;
 int sum_accel=0;
 int i=0;
 int calb;
